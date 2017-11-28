fd4f890ec9a558199835e1473338eaeeb9b3821f
Anthony Desmier <adesmier@gmail.com>
2017-11-28 17:06:18 +0000

    Authentication now uses local storage#

--- js/components/LoginForm.jsx
+++ js/components/LoginForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import Auth from '../scripts/BasicAuth.js';
-import Cookie from '../scripts/Cookies.js';
-import {OA_COOKIE_KEY, OA_COOKIE_EXPIRY} from '../scripts/Constants';
+import SessionCache from '../scripts/SessionCache.js';
+import {OA_API_KEY} from '../scripts/Constants';
 
 
 export default class LoginForm extends React.Component{
@@ -13,21 +13,67 @@ export default class LoginForm extends React.Component{
             oaUsername: {value: '', isEmptyClass: ''},
             oaPassword: {value: '', isEmptyClass: ''},
         },
-        sessionStatus: 'You don\'t have an active session. Please log in:'
+        sessionStatus: 'You don\'t have an active session. Please log in:',
+        loginStatus: {
+            message: '',
+            class: ''
+        },
+        authStatus: 0
     }
 
     componentDidMount(){
-        //check for existing session cookie
-        //if valid then notify that there's
-        //no need to login
-        let cookie = Cookie.getCookie([OA_COOKIE_KEY, OA_COOKIE_EXPIRY]);
-        console.log(cookie);
+        let sessionData = SessionCache.getSessionData(OA_API_KEY);
 
-        if(cookie[OA_COOKIE_KEY]){
-            this.setState({
-                sessionStatus: 'Your previous session is still active. No need to log in.'
+        if(sessionData){
+            let user = ''; let username = '';
+            let url = ''; let session = '';
+
+            if(sessionData.headers && sessionData.headers['x-sessionkey']){
+                session = sessionData.headers['x-sessionkey'];
+                if(sessionData.oaUrl){
+                    url = sessionData.oaUrl;
+                    if(sessionData.data[0]){
+                        user = sessionData.data[0].full_name;
+                        username = sessionData.data[0].username;
+                    }
+                }
+            } else {
+                console.error('There is no session key stored in the cached session data');
+                return;
+            }
+
+            //check if stored key is still valid
+            Auth.authenticate(true, url,username,session).then(response => {
+
+                if(response.data.error_message && response.status === 401){
+                    this.setState({
+                        sessionStatus: `Welcome back ${user}. Your previous session 
+                                        has expired. Please log in again.`,
+                        loginStatus: {
+                            message: `Existing session invalid: Session Key ${session}`,
+                            class: 'error'
+                        }
+                    });
+                } else if(response.status === 200){
+                    this.setState({
+                        sessionStatus: `Welcome back ${user}. Your previous session 
+                                        is still active. No need to login.`,
+                        loginStatus: {
+                            message: `Authentication successful: Using existing session Key ${session}`,
+                            class: 'success'
+                        },
+                        authStatus: 2
+                    });
+                }
+                console.log(response);
+                
+            }).catch(error => { //general network error
+                console.log(error);
             });
+        
+            
         }
+
     }
 
     inputChangeHandler = (evt) => {
@@ -54,6 +100,7 @@ export default class LoginForm extends React.Component{
     authRequest = () => {
         let credentials = this.state.credentials;
         let hasEmptyField = false;
+        this.submitBtn.blur();
 
         //if any input fields are empty then a class will be added
         //to alert the user to the input field
@@ -78,23 +125,40 @@ export default class LoginForm extends React.Component{
             });
         } else { //no fields empty so make auth api call
 
-            
+            this.setState({authStatus: 1});
 
-            console.log(this.state)
-            Auth.authenticate(credentials.oaUrl.value,
+            console.log(this.state);
+            Auth.authenticate(false, credentials.oaUrl.value,
                 credentials.oaUsername.value,
-                credentials.oaPassword.value).then(data => {
-                console.log('success');
-                console.log(data);
+                credentials.oaPassword.value).then(response => {
 
-                let sessionKey = data.headers['x-sessionkey'];
-                let expiry = data.headers.expires;
-                Cookie.setCookie(OA_COOKIE_KEY, sessionKey, expiry);
-                Cookie.setCookie(OA_COOKIE_EXPIRY, expiry, expiry);
+                if(response.status === 200){
+                    //session key saved in localstorage for persistence
+                    SessionCache.setSessionData(OA_API_KEY, response, credentials.oaUrl.value);
+                    let key = response.headers['x-sessionkey'];
+                    this.setState({
+                        loginStatus: {
+                            message: `Authentication successful: Session Key ${key}`,
+                            class: 'success'
+                        },
+                        authStatus: 2
+                    });
+                } else {
+                    this.setState({
+                        loginStatus: {
+                            message: `Authentication failed: ${response.data.error_message}`,
+                            class: 'error'
+                        },
+                        authStatus: 0
+                    });
+                }
 
-            }).catch(error => {
-                console.log('error');
-                console.log(error);
+                
+
+                console.log(response);
+
+            }).catch(error => { //general network error
+                console.error(error);
             });
 
         }
@@ -103,7 +167,24 @@ export default class LoginForm extends React.Component{
 
     render(){
         const {oaUrl, oaUsername, oaPassword} = this.state.credentials;
-        const {sessionStatus} = this.state;
+        const {sessionStatus, loginStatus, authStatus} = this.state;
+
+        let submitBtnContent;
+        let inputDisabled;
+        switch(authStatus){
+            case 0:
+                submitBtnContent = 'Submit';
+                inputDisabled = false;
+                break;
+            case 1:
+                submitBtnContent = <i className="fa fa-spin fa-refresh"></i>;
+                inputDisabled = true;
+                break;
+            case 2:
+                submitBtnContent = <i className="fa fa-check"></i>;
+                inputDisabled = true;
+                break;
+        }
 
         return[
             <div className="session-check">
@@ -119,7 +200,7 @@ export default class LoginForm extends React.Component{
                                placeholder="subdomain"
                                name="oaUrl"
                                onChange={evt => this.inputChangeHandler(evt)}
-                                />
+                               disabled={inputDisabled} />
                         <span>.openasset.com</span>
                     </div>
                 </div>
@@ -131,7 +212,7 @@ export default class LoginForm extends React.Component{
                                placeholder="Username"
                                name="oaUsername"
                                onChange={evt => this.inputChangeHandler(evt)}
-                               />
+                               disabled={inputDisabled} />
                     </div>
                     <div className="six columns">
                         <label htmlFor="oaPassword">OpenAsset Password</label>
@@ -139,11 +220,21 @@ export default class LoginForm extends React.Component{
                                type="password"
                                name="oaPassword"
                                onChange={evt => this.inputChangeHandler(evt)}
-                               />
+                               disabled={inputDisabled} />
                     </div>
                 </div>
-                <div className="row">
-                    <input className="button-primary" type="button" onClick={() => {this.authRequest()}} value="Submit" />
+                <div id="submit-btn-wrapper" className="row">
+                    <button className="button-primary pending"
+                            type="button"
+                            //manage button focus
+                            ref={(submitBtn) => {this.submitBtn = submitBtn}}
+                            onClick={() => {this.authRequest()}}
+                            disabled={inputDisabled}>
+                            {submitBtnContent}
+                    </button>
+                    <span className={['login-status', loginStatus.class].join(' ')}>
+                        {loginStatus.message}
+                    </span>
                 </div>
                 <hr />
             </form>
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -45,6 +45,10 @@ module.exports = {
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
             }
         ]
     },
